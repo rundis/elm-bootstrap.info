c77a5ea242b7fe17f9589c89bd4e0edce598add0
Magnus Rundberget <mrundberget@hotmail.com>
2018-10-17 16:22:22 +0200

    Change navigation example to inline code as it seems Ellie doesnt support full-page app

--- src/Page/GettingStarted.elm
+++ src/Page/GettingStarted.elm
@@ -187,22 +187,303 @@ Grid.row
 """
 
 
+
+
+
 example : List (Html msg)
 example =
     [ h2 [] [ text "Example page" ]
     , p [] [ text """In the example below we've made a super simple single page application.
                      It contains basic navigation and a couple of example pages. """ ]
-    , iframe
-        [ src "https://ellie-app.com/embed/53W4KNyCZa1/0"
-        , style "width" "100%"
-        , style "height" "600px"
-        , style "border" "0"
-        , style "border-radius" "3px"
-        , style "overflow" "hidden"
-        , attribute "sandbox" "allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
-        ]
-        []
+    , h3 [] [ text "Elm code" ]
+    , Util.code exampleElmCode
+    , h3 [] [ text "Html Index Page" ]
+    , Util.code exampleHtmlCode
     ]
+
+exampleElmCode : Html msg
+exampleElmCode =
+    Util.toMarkdownElm """
+module Main exposing (main)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Browser.Navigation as Navigation
+import Browser exposing (UrlRequest)
+import Url exposing (Url)
+import Url.Parser as UrlParser exposing ((</>), Parser, s, top)
+import Bootstrap.Navbar as Navbar
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
+import Bootstrap.Card as Card
+import Bootstrap.Card.Block as Block
+import Bootstrap.Button as Button
+import Bootstrap.ListGroup as Listgroup
+import Bootstrap.Modal as Modal
+
+
+type alias Flags =
+    {}
+
+type alias Model =
+    { navKey : Navigation.Key
+    , page : Page
+    , navState : Navbar.State
+    , modalVisibility : Modal.Visibility
+    }
+
+type Page
+    = Home
+    | GettingStarted
+    | Modules
+    | NotFound
+
+
+main : Program Flags Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = ClickedLink
+        , onUrlChange = UrlChange
+        }
+
+init : Flags -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init flags url key =
+    let
+        ( navState, navCmd ) =
+            Navbar.initialState NavMsg
+
+        ( model, urlCmd ) =
+            urlUpdate url { navKey = key, navState = navState, page = Home, modalVisibility= Modal.hidden }
+    in
+        ( model, Cmd.batch [ urlCmd, navCmd ] )
+
+
+
+
+type Msg
+    = UrlChange Url
+    | ClickedLink UrlRequest
+    | NavMsg Navbar.State
+    | CloseModal
+    | ShowModal
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Navbar.subscriptions model.navState NavMsg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedLink req ->
+             case req of
+                 Browser.Internal url ->
+                     ( model, Navigation.pushUrl model.navKey <| Url.toString url )
+
+                 Browser.External href ->
+                     ( model, Navigation.load href )
+
+
+        UrlChange url ->
+            urlUpdate url model
+
+        NavMsg state ->
+            ( { model | navState = state }
+            , Cmd.none
+            )
+
+        CloseModal ->
+            ( { model | modalVisibility = Modal.hidden }
+            , Cmd.none
+            )
+
+        ShowModal ->
+            ( { model | modalVisibility = Modal.shown }
+            , Cmd.none
+            )
+
+
+
+urlUpdate : Url -> Model -> ( Model, Cmd Msg )
+urlUpdate url model =
+    case decode url of
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
+
+        Just route ->
+            ( { model | page = route }, Cmd.none )
+
+
+decode : Url -> Maybe Page
+decode url =
+    { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
+    |> UrlParser.parse routeParser
+
+
+routeParser : Parser (Page -> a) a
+routeParser =
+    UrlParser.oneOf
+        [ UrlParser.map Home top
+        , UrlParser.map GettingStarted (s "getting-started")
+        , UrlParser.map Modules (s "modules")
+        ]
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Elm Bootstrap"
+    , body =
+        [ div []
+            [ menu model
+            , mainContent model
+            , modal model
+            ]
+        ]
+    }
+
+
+
+menu : Model -> Html Msg
+menu model =
+    Navbar.config NavMsg
+        |> Navbar.withAnimation
+        |> Navbar.container
+        |> Navbar.brand [ href "#" ] [ text "Elm Bootstrap" ]
+        |> Navbar.items
+            [ Navbar.itemLink [ href "#getting-started" ] [ text "Getting started" ]
+            , Navbar.itemLink [ href "#modules" ] [ text "Modules" ]
+            ]
+        |> Navbar.view model.navState
+
+
+mainContent : Model -> Html Msg
+mainContent model =
+    Grid.container [] <|
+        case model.page of
+            Home ->
+                pageHome model
+
+            GettingStarted ->
+                pageGettingStarted model
+
+            Modules ->
+                pageModules model
+
+            NotFound ->
+                pageNotFound
+
+
+pageHome : Model -> List (Html Msg)
+pageHome model =
+    [ h1 [] [ text "Home" ]
+    , Grid.row []
+        [ Grid.col []
+            [ Card.config [ Card.outlinePrimary ]
+                |> Card.headerH4 [] [ text "Getting started" ]
+                |> Card.block []
+                    [ Block.text [] [ text "Getting started is real easy. Just click the start button." ]
+                    , Block.custom <|
+                        Button.linkButton
+                            [ Button.primary, Button.attrs [ href "#getting-started" ] ]
+                            [ text "Start" ]
+                    ]
+                |> Card.view
+            ]
+        , Grid.col []
+            [ Card.config [ Card.outlineDanger ]
+                |> Card.headerH4 [] [ text "Modules" ]
+                |> Card.block []
+                    [ Block.text [] [ text "Check out the modules overview" ]
+                    , Block.custom <|
+                        Button.linkButton
+                            [ Button.primary, Button.attrs [ href "#modules" ] ]
+                            [ text "Module" ]
+                    ]
+                |> Card.view
+            ]
+        ]
+    ]
+
+
+pageGettingStarted : Model -> List (Html Msg)
+pageGettingStarted model =
+    [ h2 [] [ text "Getting started" ]
+    , Button.button
+        [ Button.success
+        , Button.large
+        , Button.block
+        , Button.attrs [ onClick ShowModal ]
+        ]
+        [ text "Click me" ]
+    ]
+
+
+pageModules : Model -> List (Html Msg)
+pageModules model =
+    [ h1 [] [ text "Modules" ]
+    , Listgroup.ul
+        [ Listgroup.li [] [ text "Alert" ]
+        , Listgroup.li [] [ text "Badge" ]
+        , Listgroup.li [] [ text "Card" ]
+        ]
+    ]
+
+
+pageNotFound : List (Html Msg)
+pageNotFound =
+    [ h1 [] [ text "Not found" ]
+    , text "SOrry couldn't find that page"
+    ]
+
+
+modal : Model -> Html Msg
+modal model =
+    Modal.config CloseModal
+        |> Modal.small
+        |> Modal.h4 [] [ text "Getting started ?" ]
+        |> Modal.body []
+            [ Grid.containerFluid []
+                [ Grid.row []
+                    [ Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 1" ]
+                    , Grid.col
+                        [ Col.xs6 ]
+                        [ text "Col 2" ]
+                    ]
+                ]
+            ]
+        |> Modal.view model.modalVisibility
+
+"""
+
+exampleHtmlCode : Html msg
+exampleHtmlCode =
+    Util.toMarkdown """
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      </head>
+      <body>
+        <script>
+            var app = Elm.Main.init({})
+            // you can use ports and stuff here
+        </script>
+      </body>
+    </html>
+"""
 
 
 textLi : String -> Html msg
